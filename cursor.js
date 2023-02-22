61ccee8e449076ceac92392cff61dcb17668b4df
Thomas Foret <thomas.foret@etu.univ-grenoble-alpes.fr>
2023-02-22 16:13:37 +0100

    placement des boutons à équidistance de start

--- cursor.js
+++ cursor.js
@@ -9,9 +9,27 @@ function showCursor(){
 
 }
 
-el = document.getElementById("start");
-console.log(el)
-el.addEventListener('click',hideCursor,false); //!Erreur
+function elementPositionL (a) { //renvoie la position coin haut gauche d'un objet
+    return {
+      clientX: a.offsetLeft,
+      clientY: a.offsetTop,
+    }
+  }
+
+function showPos(a,){ //affiche la distance entre le coin haut gauche du bouton start au coin haut gauche des boutons de gauche
+    let elb = document.getElementById('bleu');
+    let elr = document.getElementById('rouge');
+    let els = document.getElementById('start');
+    let pb = elementPositionL(elb)
+    let pr = elementPositionL(elr)
+    let psL = elementPositionL(els)
+    console.log((psL.clientX-pb.clientX)+(psL.clientY-pb.clientY))
+    console.log((psL.clientX-pr.clientX)+(psL.clientY-pr.clientY))
+}
+
+//el.addEventListener('click',hideCursor); //!Erreur
+
+
 
 function startButton(){
     //inserer toutes les actions
