b66a1d4237c7b9c82fd9bcab5b4358397ea560c4
Thomas Foret <thomas.foret@etu.univ-grenoble-alpes.fr>
2023-03-04 17:16:06 +0100

    création menu avec consentement

--- text.js
+++ text.js
@@ -8,5 +8,14 @@ function addText(){
     else{
         document.getElementById("text").innerHTML = " ";
     }
+}
 
+function hideMenu(){ //cache le menu et active le bouton de l'expérience
+    if(document.getElementById("check").checked){//si checkbox cochée
+        document.getElementById("menu").style.display="none";
+        document.getElementById('start').disabled = false;
+    }
+    else{//si checkbox décochée
+       alert("Vous devez d'abord confirmer votre consentement pour lancer l'expérience.")
+    }
 }
