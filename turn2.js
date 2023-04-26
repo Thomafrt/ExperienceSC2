b7c16a3ca71a965c31ea73900a484dbccea92a35
Noe_grvrd <91073435+NoGravd@users.noreply.github.com>
2023-04-26 17:58:59 +0200

    Add files via upload

--- turn2.js
+++ turn2.js
@@ -1,8 +1,8 @@
 import {addText, deleteText, addError, addColor, hideMenu, showPauseMenu, hideStart, showStart} from './text.js';
 import {generateBlockRandom, generateBlockSequence, generateBlockTest} from './word.js';
-import { recordMouse, stopMouseAndGetFrames} from './recorder.js';
+import { recordMouse, stopMouseAndGetFrames} from './mouse_recorder.js';
 
-const data=[];//le tableau au tout est stocké pour être converti en Json
+const data=[];//le tableau ou tout est stocké pour être converti en Json
 
 
 /**
@@ -41,6 +41,7 @@ function turn(congrence, trials, mode){
 
         //enregistrement des data
         recordMouse();
+        start_recording();
         //! lancer enregistrement du temps si besoin
     });
 }
@@ -98,43 +99,43 @@ export function addEventExpe(color, nb){ //! ajouter la fonction fin d'enregistr
     let signal = {signal : controller.signal};
     if(color=="red"){
         rouge.addEventListener('click', () => {//la bonne réponse
-            deleteText(); controller.abort(); showStart(500); showPauseMenu(nb); addAnswer(true);}, signal);
+            deleteText(); controller.abort(); showStart(500); showPauseMenu(nb); addAnswer(true); end_trial(false);}, signal);
         bleu.addEventListener('click', () => {
-            addError(); controller.abort(); showStart(2000); showPauseMenu(nb); addAnswer(false);}, signal);
+            addError(); controller.abort(); showStart(2000); showPauseMenu(nb); addAnswer(false); end_trial(true);}, signal);
         jaune.addEventListener('click', () => {
-            addError(); controller.abort(); showStart(2000); showPauseMenu(nb); addAnswer(false);}, signal);
+            addError(); controller.abort(); showStart(2000); showPauseMenu(nb); addAnswer(false); end_trial(true);}, signal);
         vert.addEventListener('click', () => {
-            addError(); controller.abort(); showStart(2000); showPauseMenu(nb); addAnswer(false);}, signal);
+            addError(); controller.abort(); showStart(2000); showPauseMenu(nb); addAnswer(false); end_trial(true);}, signal);
     }
     else if (color=="blue"){
         rouge.addEventListener('click', () => {
-            addError(); controller.abort(); showStart(2000); showPauseMenu(nb); addAnswer(false);}, signal);
+            addError(); controller.abort(); showStart(2000); showPauseMenu(nb); addAnswer(false); end_trial(true);}, signal);
         bleu.addEventListener('click', () => {//la bonne réponse
-            deleteText(); controller.abort(); showStart(500); showPauseMenu(nb); addAnswer(true);}, signal);
+            deleteText(); controller.abort(); showStart(500); showPauseMenu(nb); addAnswer(true); end_trial(false);}, signal);
         jaune.addEventListener('click', () => {
-            addError(); controller.abort(); showStart(2000); showPauseMenu(nb); addAnswer(false);}, signal);
+            addError(); controller.abort(); showStart(2000); showPauseMenu(nb); addAnswer(false); end_trial(true);}, signal);
         vert.addEventListener('click', () => {
-            addError(); controller.abort(); showStart(2000); showPauseMenu(nb); addAnswer(false);}, signal);
+            addError(); controller.abort(); showStart(2000); showPauseMenu(nb); addAnswer(false); end_trial(true);}, signal);
     }
     else if (color=="yellow"){
         rouge.addEventListener('click', () => {
-            addError(); controller.abort(); showStart(2000); showPauseMenu(nb); addAnswer(false);}, signal);
+            addError(); controller.abort(); showStart(2000); showPauseMenu(nb); addAnswer(false); end_trial(true);}, signal);
         bleu.addEventListener('click', () => {
-            addError(); controller.abort(); showStart(2000); showPauseMenu(nb); addAnswer(false);}, signal);
+            addError(); controller.abort(); showStart(2000); showPauseMenu(nb); addAnswer(false); end_trial(true);}, signal);
         jaune.addEventListener('click', () => {//la bonne réponse
-            deleteText(); controller.abort(); showStart(500); showPauseMenu(nb); addAnswer(true);}, signal);
+            deleteText(); controller.abort(); showStart(500); showPauseMenu(nb); addAnswer(true); end_trial(false);}, signal);
         vert.addEventListener('click', () => {
-            addError(); controller.abort(); showStart(2000); showPauseMenu(nb); addAnswer(false);}, signal);
+            addError(); controller.abort(); showStart(2000); showPauseMenu(nb); addAnswer(false); end_trial(true);}, signal);
     }
     else if (color=="green"){
         rouge.addEventListener('click', () => {
-            addError(); controller.abort(); showStart(2000); showPauseMenu(nb); addAnswer(false);}, signal);
+            addError(); controller.abort(); showStart(2000); showPauseMenu(nb); addAnswer(false); end_trial(true);}, signal);
         bleu.addEventListener('click', () => {
-            addError(); controller.abort(); showStart(2000); showPauseMenu(nb); addAnswer(false);}, signal);
+            addError(); controller.abort(); showStart(2000); showPauseMenu(nb); addAnswer(false); end_trial(true);}, signal);
         jaune.addEventListener('click', () => {
-            addError(); controller.abort(); showStart(2000); showPauseMenu(nb); addAnswer(false);}, signal);
+            addError(); controller.abort(); showStart(2000); showPauseMenu(nb); addAnswer(false); end_trial(true);}, signal);
         vert.addEventListener('click', () => {//la bonne réponse
-            deleteText(); controller.abort(); showStart(500); showPauseMenu(nb); addAnswer(true);}, signal);
+            deleteText(); controller.abort(); showStart(500); showPauseMenu(nb); addAnswer(true); end_trial(false);}, signal);
     }
 }
 
@@ -148,3 +149,72 @@ function addAnswer(type){
 
 //MAIN
  turn(0.8, 40, 2);
+
+
+
+
+
+var mouse_info = [];
+var failed_trial= [];
+var times = [];
+var AUC = [];
+
+function end_trial(as_failed_trial) {
+    failed_trial.push(as_failed_trial);
+    mouse_info.push(stopMouseAndGetFrames());
+    console.log("mouse info : " + mouse_info);
+    AUC.push(stop_recording());
+    times.push(Date.now()-time_start);
+}
+
+
+ var points;
+ var time_start;
+
+ function start_recording() {
+    // Réinitialisation de la liste des points de la sourie
+    points = { X: [], Y: [], times: [] };
+    time_start = Date.now();
+    window.addEventListener("mousemove", calculate_event_AUC);
+}
+
+function stop_recording() {
+    window.removeEventListener("mousemove", calculate_event_AUC);
+    return calculate_AUC(points);
+}
+
+function calculate_event_AUC(event) {
+    let current_time = Date.now();
+    points.X.push(event.clientX);
+    points.Y.push(event.clientY);
+    points.times.push((current_time-time_start));              
+}
+
+function calculate_AUC(points) {
+    // Obtenir les coordonnées des points de départ et d'arrivée
+    const startX = points.X[0];
+    const startY = points.Y[0];
+    const endX = points.X[points.X.length - 1];
+    const endY = points.Y[points.Y.length - 1];
+    
+    // Calculer la distance en ligne droite
+    const distance = Math.sqrt((endX - startX) ** 2 + (endY - startY) ** 2);
+    
+    // Calculer Air Under Curve
+    let area = 0;
+    for (let i = 1; i < points.X.length; i++) {
+        const x1 = points.X[i - 1];
+        const y1 = points.Y[i - 1];
+        const x2 = points.X[i];
+        const y2 = points.Y[i];
+        const dx = x2 - x1;
+        const dy = y2 - y1;
+        const distanceToLine = (dx === 0 && dy === 0) ? 0 : Math.abs(dy * startX - dx * startY + x2 * y1 - y2 * x1) / Math.sqrt(dx ** 2 + dy ** 2);
+
+        //const distanceToLine = Math.abs(dy * startX - dx * startY + x2 * y1 - y2 * x1) / Math.sqrt(dx ** 2 + dy ** 2);
+        area += distanceToLine;
+    }
+
+    const AUC = area / distance;
+    return AUC; 
+}
